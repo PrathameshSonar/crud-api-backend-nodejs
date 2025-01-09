3ae0ada4b8c230bfa05dfe1939f2096855a36621
Prathamesh Sonar <45892874+PrathameshSonar@users.noreply.github.com>
2025-01-09 12:58:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require('./model/product.model');
+const productRoutes = require('./routes/product.routes')
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+//routes
+app.use("/api/products/", productRoutes)
+
+app.get("/", (req, res) => {
+  console.log(res.send("xyz"));
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://pms:prathamesh@cluster0.6kfba.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("DB connection success");
+    app.listen(3000, () => {
+        console.log("Server is running");
+      });
+  })
+  .catch((error) => {
+    console.log("DB connection failed", error);
+  });
